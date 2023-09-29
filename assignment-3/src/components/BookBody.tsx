1870ad8206290581a51f34630205a7bc7c342efe
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-09-29 23:34:15 +0700

    test commit

--- assignment-3/src/components/BookBody.tsx
+++ assignment-3/src/components/BookBody.tsx
@@ -21,6 +21,7 @@ export default function BookBody() {
   const beautifulTableClass = 'border-2 border-[#c5cfd9] p-2'
   const a = 'a'
   const b = 'a'
+  const c = 'a'
   const { appSummaryInfo, dataChanged, editInputValue, changeDataChanged } =
     useAppContext()
   const [currentPage, setCurrentPage] = useState(1)
