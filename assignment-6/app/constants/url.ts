40be191dbe8da807560b94048c79145b915038ff
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-10-13 23:02:40 +0700

    test hw7 in hw6

--- assignment-6/app/constants/url.ts
+++ assignment-6/app/constants/url.ts
@@ -1,2 +1,4 @@
 export const BASE_URL = 'https://develop-api.bookstore.dwarvesf.com/api/v1';
 export const URL_BOOKS = `${BASE_URL}/books`;
+export const DOC_JSON_URL =
+  'https://develop-api.bookstore.dwarvesf.com/swagger/doc.json';
